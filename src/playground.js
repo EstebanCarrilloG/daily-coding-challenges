985082aa600fdad1458778dd6524bf4568eed90f
EstebanCarrilloG <estexav0996@gmail.com>
2025-10-30 11:38:33 -0500

    add: update test script and add playground test file

---
+++ src/playground.js
@@ -0,0 +1,2 @@
+
+//module.exports.testFunction = /* function */; 
---
+++ src/playground.test.js
@@ -0,0 +1,5 @@
+const { testFunction } = require("./playground");
+
+test("", () => {
+  expect(1).toEqual(1); //expect(testFunction())
+});
